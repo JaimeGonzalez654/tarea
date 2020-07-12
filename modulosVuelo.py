b56d0c8294d8c53a55e1a1beb490221eac1761a9
JaimeGonzalez654 <63812876+JaimeGonzalez654@users.noreply.github.com>
2020-07-12 14:09:51 -0400

    Update modulosVuelo.py

--- modulosVuelo.py
+++ modulosVuelo.py
@@ -1,16 +1,21 @@
 def mincomb(estadia):
+    tiempo = 0
     while estadia < 0:
         estadia = int(input("ingrese un valor valido para su estadia: "))
-    if estadia <= 480:
+    while estadia <= 480:
         mincomb = 0
-    if estadia > 480:
-        for i in range (1,estadia,480):
+    while estadia > 480:
+        for i in range (1,estadia):
+            tiempo = tiempo + 480
+            while estadia < tiempo:
                 mincomb = (5000*i)
-        return mincomb
+    return resultado
 
 ### preograma principal ###
 x = int(input("Hola! cuantos minutos se quedará?: "))
-print (mincomb(x))
+resultado = (mincomb(x))
+print(resultado)
+
 
 
 
